409faf66b05010f15ff80eef4912c6fdd57f232a
Adam Hewitt <adamhewitt@olivetree.com>
2018-01-15 22:54:59 -0800

    Revert "remove non-uap runtimes"
    This reverts commit b1d0649bf41b02c31309c3a414be7743c23ef887.

--- getSQLite.ps1
+++ getSQLite.ps1
@@ -1,14 +1,13 @@
 try {
     Push-Location $PSScriptRoot
-    Remove-Item runtimes -Recurse
     $year = "2017"
     $version = "3210000"
 
-    &{ <# Win32
+    &{ #<# Win32
         function Get-Win32 ($win, $arch) {
             Invoke-WebRequest https://sqlite.org/$year/sqlite-dll-$win-$arch-$version.zip -OutFile sqlite.zip
             Expand-Archive .\sqlite.zip -Force
-            $dest = New-Item "contentFiles\netcoreapp2.0\$arch" -ItemType Directory -Force
+            $dest = New-Item "runtimes\win-$arch\lib\netstandard1.4" -ItemType Directory -Force
             Move-Item "sqlite\sqlite3.dll" -Destination $dest -Force            
         }
         Get-Win32 win32 x86
@@ -19,7 +18,7 @@ try {
         Invoke-WebRequest https://sqlite.org/$year/sqlite-uwp-$version.vsix -OutFile sqlite.zip
         Expand-Archive .\sqlite.zip -Force
         Get-ChildItem sqlite\Redist\Retail | ForEach-Object {
-            $dest = New-Item "runtimes\win10-$($_.Name.ToLowerInvariant())\lib\uap10.0" -ItemType Directory -Force
+            $dest = New-Item "runtimes\win-$($_.Name.ToLowerInvariant())\lib\uap10.0" -ItemType Directory -Force
             Move-Item (Join-Path $_.FullName sqlite3.dll) -Destination $dest -Force
         }
     <##>}
