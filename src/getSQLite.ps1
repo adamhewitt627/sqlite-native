6a1820331399a81067dc4c11d4c9a796637942ff
Adam Hewitt <adamhewitt@olivetree.com>
2018-01-30 12:35:41 -0800

    Add versioned appveyor build

--- src/getSQLite.ps1
+++ src/getSQLite.ps1
@@ -1,14 +1,20 @@
 try {
     Push-Location $PSScriptRoot
-    $year = "2018"
-    $version = "3220000"
+    $buildVersion = [version]([regex]"version: (\d+.\d+.\d+)").Match((Get-Content ..\appveyor.yml -Raw)).Groups[1].Value
+    $version = @($buildVersion.Major.ToString(), $buildVersion.Minor.ToString().PadRight(3,'0'), $buildVersion.Build.ToString().PadRight(3,'0')) -join ""
+
+    $year = (Get-Date).Year + 1
+    do { try {
+        Invoke-WebRequest "https://sqlite.org/$year/sqlite-amalgamation-$version.zip" -Method Head | Out-Null
+        break
+    } catch { $year-- } } while ($true)
 
     &{ #<# Win32
         function Get-Win32 ($win, $arch) {
             Invoke-WebRequest https://sqlite.org/$year/sqlite-dll-$win-$arch-$version.zip -OutFile sqlite.zip
             Expand-Archive .\sqlite.zip -Force
             $dest = New-Item "runtimes\win-$arch\lib\netstandard1.4" -ItemType Directory -Force
-            Move-Item "sqlite\sqlite3.dll" -Destination $dest -Force            
+            Move-Item "sqlite\sqlite3.dll" -Destination $dest -Force
         }
         Get-Win32 win32 x86
         Get-Win32 win64 x64
