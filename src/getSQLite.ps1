f4ab686fe779fc8957241cf76c377766021431f3
Adam Hewitt <adamhewitt@olivetree.com>
2018-02-09 14:20:55 -0800

    try putting windows libs in a netcoreapp folder

--- src/getSQLite.ps1
+++ src/getSQLite.ps1
@@ -13,7 +13,7 @@ try {
         function Get-Win32 ($win, $arch) {
             Invoke-WebRequest https://sqlite.org/$year/sqlite-dll-$win-$arch-$version.zip -OutFile sqlite.zip
             Expand-Archive .\sqlite.zip -Force
-            $dest = New-Item "runtimes\win-$arch\native" -ItemType Directory -Force
+            $dest = New-Item "runtimes\win-$arch\netcoreapp2.0" -ItemType Directory -Force
             Move-Item "sqlite\sqlite3.dll" -Destination $dest -Force
         }
         Get-Win32 win32 x86
