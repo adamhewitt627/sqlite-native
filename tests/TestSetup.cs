74b42b17946360f9b7a051572590608bd83c39af
Adam Hewitt <adamhewitt@olivetree.com>
2018-02-10 21:40:30 -0800

    enable x86 runtime

--- tests/TestSetup.cs
+++ tests/TestSetup.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SqliteNative.Tests
 {
-    /*[TestClass]
+    [TestClass]
     public class TestSetup
     {
         [DllImport("kernel32")]
@@ -16,9 +16,9 @@ namespace SqliteNative.Tests
         {
             string @base = System.IO.Path.GetDirectoryName(new Uri(typeof(TestSetup).GetTypeInfo().Assembly.CodeBase).LocalPath),
                 bitness = IntPtr.Size == 8 ? "x64" : "x86",
-                file = $@"win-{bitness}\native\sqlite3.dll";
+                file = $@"{bitness}\sqlite3.dll";
             var lib = LoadLibrary(System.IO.Path.Combine(@base, file));
             Assert.AreNotEqual(lib, IntPtr.Zero);
         }
-    }*/
+    }
 }
