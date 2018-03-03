ab2c74fb2804d5fdf8a2f0953da2924eae7d60e5
Adam Hewitt <adamhewitt@olivetree.com>
2018-03-03 14:40:05 -0800

    Start of Database and Statement native wrappers

---
+++ src/Database.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Logging;
+using SqliteNative.Util;
+using System;
+using static SqliteNative.Sqlite3;
+
+namespace SqliteNative
+{
+    public enum OpenFlags
+    {
+        ReadOnly = SQLITE_OPEN_READONLY,
+        ReadWrite = SQLITE_OPEN_READWRITE,
+        Create = SQLITE_OPEN_CREATE,
+        Uri = SQLITE_OPEN_URI,
+        Memory = SQLITE_OPEN_MEMORY,
+        NoMutex = SQLITE_OPEN_NOMUTEX,
+        FullMutex = SQLITE_OPEN_FULLMUTEX,
+        SharedCache = SQLITE_OPEN_SHAREDCACHE,
+        PrivateCache = SQLITE_OPEN_PRIVATECACHE,
+    }
+
+    internal class Database : Disposable
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger<Database> _logger;
+        private IntPtr _db;
+
+        public Database(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+            _logger = loggerFactory.CreateLogger<Database>();
+        }
+
+        protected override void Dispose(bool disposing) => Close();
+        public static implicit operator IntPtr(Database database) => database._db;
+
+        public bool Open(string filename, OpenFlags flags)
+        {
+            var err = sqlite3_open_v2(filename, out _db, (int)flags);
+            if (err != SQLITE_OK) _logger.LogError(ErrorMessage);
+            return err == SQLITE_OK;
+        }
+
+        public bool Close()
+        {
+            var err = sqlite3_close_v2(_db);
+            if (err != SQLITE_OK) _logger.LogError(ErrorMessage);
+            return err == SQLITE_OK;
+        }
+
+        public Statement Prepare(string sql) => Prepare(sql, out var remaining);
+        public Statement Prepare(string sql, out string remaining) => new Statement(_loggerFactory, this, sql, out remaining);
+        public bool Execute(string sql)
+        {
+            var err = sqlite3_exec(this, sql);
+            if (err != SQLITE_OK) _logger.LogError(ErrorMessage);
+            return err == SQLITE_OK;
+        }
+
+        public string ErrorMessage => sqlite3_errmsg(this);
+        public string ErrorString => sqlite3_errstr(this);
+        public int ErrorCode => sqlite3_errcode(this);
+        public int ExtendedErrorCode => sqlite3_extended_errcode(this);
+
+        public int Changes => sqlite3_changes(this);
+        public int TotalChanges => sqlite3_total_changes(this);
+        public long LastInsertedRowId => sqlite3_last_insert_rowid(this);
+    }
+}
---
+++ src/Statement.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using SqliteNative.Util;
+using System;
+using static SqliteNative.Sqlite3;
+
+namespace SqliteNative
+{
+    internal class Statement : Disposable
+    {
+        private readonly ILogger<Statement> _logger;
+        private IntPtr _stmt;
+        private Database _database;
+
+        public Statement(ILoggerFactory loggerFactory, Database database, string sql, out string remaining)
+        {
+            if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
+            _logger = loggerFactory.CreateLogger<Statement>();
+
+            _database = database ?? throw new ArgumentNullException(nameof(database));
+            if (!(sqlite3_prepare16_v2(database, sql, out _stmt, out remaining) is SQLITE_OK))
+                _logger.LogError(_database.ErrorMessage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            var err = sqlite3_finalize(_stmt);
+            if (err != SQLITE_OK && disposing)
+                _logger.LogError(_database.ErrorMessage);
+        }
+
+        public static implicit operator IntPtr(Statement statement) => statement._stmt;
+    }
+}
