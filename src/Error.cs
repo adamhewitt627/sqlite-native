b4162d9c1f1668b62380440100cc00ae704ae218
Adam Hewitt <adamhewitt@olivetree.com>
2019-01-11 10:59:08 -0800

    Remove bad wrapping API

--- src/Error.cs
+++ src/Error.cs
@@ -6,7 +6,6 @@ namespace SqliteNative
     public interface IError
     {
         string Message { get; }
-        string String { get; }
         int Code { get; }
         int ExtendedCode { get; }
     }
@@ -16,7 +15,6 @@ namespace SqliteNative
         private Database _database;
         public Error(Database database) => _database = database ?? throw new ArgumentNullException(nameof(database));
         public string Message => sqlite3_errmsg(_database);
-        public string String => sqlite3_errstr(_database);
         public int Code => sqlite3_errcode(_database);
         public int ExtendedCode => sqlite3_extended_errcode(_database);
     }
